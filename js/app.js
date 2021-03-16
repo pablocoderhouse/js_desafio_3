fb7efaae424ed70dec51924a40eb1a165f83d10e
Pablo Gonzalez <you@example.com>
2021-03-16 16:07:18 -0300

    desafio 3 JS

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//let i= prompt("imgrese un numero")
+//while(i <= 5) {
+ //   console.log("hola");
+   // i++;
+//}
+let a = prompt("ingrese un numero")
+
+for (; a <= 50; a++) {
+    //Si la variable i es 5, no se interpreta la repetición
+ 
+   
+    if(a >=17 && a <=39) {
+        console.log ("no me gustan estos numeros")
+    continue; 
+}
+    
+    console.log(a);
+}
+
